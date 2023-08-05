11317809898540a968e7356f3ada10a57842bcc4
mtalli <mauno.talli@hotmail.com>
2023-08-05 08:37:01 +0300

    JS working, still need to add read/not read functionality

--- library.js
+++ library.js
@@ -20,9 +20,29 @@ function addBookToLibrary() {
    if (title && author && pages) {
     const newBook = new Book(title, author, pages);
     myLibrary.push(newBook);
+
+    titleInput.value = '';
+    authorInput.value = '';
+
+    displayLibrary();
    }
 }
   
+function displayLibrary() {
+  const bookList = document.querySelector('.book-list');
+  bookList.innerHTML = '';
+
+  myLibrary.forEach(book => {
+    const bookInfo = document.createElement('div');
+    bookInfo.textContent = `${book.title} by ${book.author} ${book.pages} pages long`;
+    bookList.appendChild(bookInfo);
+  })
+}
+
+const addButton = document.getElementById('add-book');
+addButton.addEventListener('click', addBookToLibrary);
+
+displayLibrary();
 
   Book.prototype.info = function() {
     const readStatus = this.read ? "read" : "not read yet";
