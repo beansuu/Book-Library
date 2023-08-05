c72a32e9bc589d8bd9d3826f02adb62d72a205a5
mtalli <mauno.talli@hotmail.com>
2023-08-05 07:55:28 +0300

    Added function to add books to myLibrary array

--- library.js
+++ library.js
@@ -1,16 +1,31 @@
+let myLibrary = [];
+
 function Book(title, author, pages, read) {
     this.title = title;
     this.author = author;
     this.pages = pages;
     this.read = read;
-  }
+  };
+
+
+function addBookToLibrary() {
+   const titleInput = document.getElementById('title');
+   const authorInput = document.getElementById('author');
+   const pagesInput = document.getElementById('pages');
+   
+   const title = titleInput.value;
+   const author = authorInput.value;
+   const pages = pagesInput.value;
+
+   if (title && author && pages) {
+    const newBook = new Book(title, author, pages);
+    myLibrary.push(newBook);
+   }
+}
   
+
   Book.prototype.info = function() {
     const readStatus = this.read ? "read" : "not read yet";
     return `${this.title} by ${this.author}, ${this.pages} pages, ${readStatus}`;
   };
   
-  // Creating instances of Book
-  const theHobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, false);
-
-  console.log(theHobbit.info())
